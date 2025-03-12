f4d61b100316020bc7e1f20524d4e66f17617462
Paul-99-art <karaneugenepetrova@gmail.com>
2025-03-11 21:33:38 -0700

    setup native tailwindcss

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -10,6 +10,9 @@ export default function Index() {
         alignItems: "center",
       }}
     >
+      <Text className="text-2xl font-semibold text-gray-800 text-center my-10">
+  Welcome to Vancouver's Real Estate
+</Text>
       <Link href="/sign-in">Sign in</Link>
       <Link href="/explore">Explore</Link>
       <Link href="/profile">Profile</Link>

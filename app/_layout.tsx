d655c3797c7b9d860c01112a59753abf7aab47c6
Paul-99-art <karaneugenepetrova@gmail.com>
2025-03-12 22:56:07 -0700

    Assets folder wasn't  properly so try adding fonts again

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,25 @@
-import { Stack } from "expo-router";
+import { useEffect } from 'react'; // Import useEffect
+import { SplashScreen, Stack } from "expo-router";
+import { useFonts } from "expo-font";
 import "./globals.css";
+
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    "Rubik-Bold": require('../assets/fonts/Rubik-Bold.ttf'),
+    "Rubik-Regular": require('../assets/fonts/Rubik-Regular.ttf'),
+    "Rubik-Medium": require('../assets/fonts/Rubik-Medium.ttf'),
+    "Rubik-Light": require('../assets/fonts/Rubik-Light.ttf'),
+    "Rubik-ExtraBold": require('../assets/fonts/Rubik-ExtraBold.ttf'),
+    "Rubik-SemiBold": require('../assets/fonts/Rubik-SemiBold.ttf'),
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hide();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) return null;
+
   return <Stack />;
 }

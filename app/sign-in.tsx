c5b1672d9db5f779dd31992ec797aa25c2d4acff
Paul-99-art <karaneugenepetrova@gmail.com>
2025-03-13 19:35:46 -0700

    UI for sign screen done

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,12 +1,34 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+import images from '../constants/images'
+import icons from '../constants/icons'
 
 const signin = () => {
+  const handleLogin = () =>{};
   return (
-    <View>
-      <Text>sign-in</Text>
+    <SafeAreaView className="bg-white h-full">
+      <ScrollView contentContainerClassName="h-full"> 
+        <Image source={images.onboarding} className="w-full h-4/6" resizeMode="contain" />
+      
+  <View className="px-10">
+  <Text className="text-base text-center uppercase font-rubik text-black-200">Welcome to Vancouver Homes</Text>
+  <Text className="text-3xl font-rubik-bold text-black-300 text-center mt-2">Your dream home awaits!{'\n'}
+  <Text className="text-primary-300">Your Ideal Home</Text>
+  </Text>
+  <Text className="text-lg font-rubik text-black-200 text-center mt-10">
+    Login to Vancouver Realestate with Google
+  </Text>
+  <TouchableOpacity onPress={handleLogin} className='bg-white shadow-md shadow-zinc-300 rounded-full w-full py-4 mt-5'>
+    <View className='flex flex-row items-center justify-center '>
+    <Image source={icons.google} className='w-5 h5' resizeMode="contain" />
+    <Text className='text-lg font-rubik-bold text-black-300 ml-2'>Sign in with Google</Text>
     </View>
+  </TouchableOpacity>
+  </View>
+  </ScrollView>
+    </SafeAreaView>
   )
 }
-
 export default signin
